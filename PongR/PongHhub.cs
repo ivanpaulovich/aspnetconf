bea2a10c75b0437e50dbcec056e4bd370934ec42
ivanpaulovich <ivan@100loop.com>
2014-05-04 16:25:29 -0300

    Demos

---
+++ PongR/PongHhub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace PongR
+{
+    public class PongHhub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
