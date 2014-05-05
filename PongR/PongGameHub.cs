329e3fe3c45711a64e35c102881d6e7a4266217d
ivanpaulovich <ivan@100loop.com>
2014-05-04 21:18:14 -0300

    PlayerOne

---
+++ PongR/PongGameHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace PongR
+{
+    public class PongGameHub : Hub
+    {
+        public void MovePlayerOne(float vPos)
+        {
+            Clients.All.updatePositionPlayerOne(vPos);
+        }
+
+        public void MovePlayerTwo(float vPos)
+        {
+            Clients.All.updatePositionPlayerTwo(vPos);
+        }
+    }
+}
